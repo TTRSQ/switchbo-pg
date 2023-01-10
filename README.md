4d73acc944d7ddbeb109fc8af0262112f5bf40a2
TTRSQ <tst.ttrsq.123@gmail.com>
2023-01-10 12:31:30 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # switchbo-pg
 sandbox
+## USAGE
+- 環境変数ファイルを作成する `cp .env.tmpl .env`
+- .env ファイルの `API_TOKEN`, `API_SECRET` をswitchbotアプリから入れる
+- 依存ライブラリをインストールする `pip install -r requirements.txt`
+- 実行する `python main.py`
---
+++ main.py
@@ -0,0 +1,85 @@
+import time
+import hashlib
+import hmac
+import base64
+import httpx
+import dotenv
+import os
+from enum import Enum
+
+dotenv.load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+API_SECRET = os.getenv("API_SECRET")
+BOT_API_HOST = "https://api.switch-bot.com"
+
+
+def create_auth_headers():
+    token = API_TOKEN
+    secret = API_SECRET
+
+    nonce = ""
+    t = int(round(time.time() * 1000))
+    string_to_sign = "{}{}{}".format(token, t, nonce)
+
+    string_to_sign = bytes(string_to_sign, "utf-8")
+    secret = bytes(secret, "utf-8")
+
+    sign = base64.b64encode(
+        hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest()
+    )
+
+    return {
+        "Authorization": token,
+        "t": str(t),
+        "sign": str(sign, "utf-8"),
+        "nonce": nonce,
+    }
+
+
+class Method(Enum):
+    GET = 1
+    POST = 2
+
+
+def request_bot_api(path: str, method: Method, param: dict = {}):
+    headers = create_auth_headers()
+    res = None
+    if method == Method.GET:
+        res = httpx.get(BOT_API_HOST + path, headers=headers, params=param)
+    elif method == Method.POST:
+        headers["Content-Type"] = "application/json; charset=utf8"
+        res = httpx.post(BOT_API_HOST + path, headers=headers, params=param)
+    return res.json()
+
+
+devices = request_bot_api("/v1.1/devices", Method.GET)["body"]["deviceList"]
+
+# 湿度計のidを取得
+meter_plus_device_id = ""
+for device in devices:
+    if device["deviceType"] == "MeterPlus":
+        meter_plus_device_id = device["deviceId"]
+        break
+
+print(meter_plus_device_id)
+print(request_bot_api(f"/v1.1/devices/{meter_plus_device_id}/status", Method.GET))
+scenes = request_bot_api("/v1.1/scenes", Method.GET)["body"]
+
+# 加湿器をコントロールするsceneの取得
+humidifier_on_scene_id = ""
+humidifier_off_scene_id = ""
+for scene in scenes:
+    if scene["sceneName"] == "加湿器ON":
+        humidifier_on_scene_id = scene["sceneId"]
+    if scene["sceneName"] == "加湿器OFF":
+        humidifier_off_scene_id = scene["sceneId"]
+
+print(humidifier_on_scene_id)
+print(humidifier_off_scene_id)
+
+# 加湿器をONにする
+print(request_bot_api(f"/v1.1/scenes/{humidifier_on_scene_id}/execute", Method.POST))
+
+time.sleep(10)
+# 加湿器をOFFにする
+print(request_bot_api(f"/v1.1/scenes/{humidifier_off_scene_id}/execute", Method.POST))
